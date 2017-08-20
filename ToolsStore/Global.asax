9fc9d3ba6aef3aea92650d6dc259df3960e60a3a
BranislavMacosinec <branomps@gmail.com>
2017-08-20 15:14:58 +0100

    Add project files.

---
+++ ToolsStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToolsStore.MvcApplication" Language="C#" %>
---
+++ ToolsStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToolsStore.Startup))]
+namespace ToolsStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
