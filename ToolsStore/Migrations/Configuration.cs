4a8a8abbe78a4d4e0ab0f108d4c64084a949ffe0
BranislavMacosinec <branomps@gmail.com>
2017-08-21 23:06:39 +0100

    Add One Product To seed method.

--- ToolsStore/Migrations/Configuration.cs
+++ ToolsStore/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace ToolsStore.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using ToolsStore.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ToolsStore.Models.ToolsStoreDB>
     {
@@ -14,6 +15,21 @@ namespace ToolsStore.Migrations
 
         protected override void Seed(ToolsStore.Models.ToolsStoreDB context)
         {
+
+
+            context.Products.AddOrUpdate(
+                p => p.ProductId,
+                new Product
+                {
+                    ProductId = 1,
+                    BrandId = 2,
+                    CategoryId= 4,
+                    Title = "Makita DKP180Z",
+                    Price = 179.99M,
+                    ProductImgUrl= "~/Images/Makita"
+
+                }
+                );
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
