2f86a30987b2f0e569f560ec98fd8b24624c216c
BranislavMacosinec <branomps@gmail.com>
2017-08-21 19:27:36 +0100

    Add StoreManager contoller with view using entity framework.

---
+++ ToolsStore/Models/ToolsStoreDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToolsStore.Models
+{
+    public class ToolsStoreDB : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ToolsStoreDB() : base("name=ToolsStoreDB")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+    
+    }
+}
