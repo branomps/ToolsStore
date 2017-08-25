b5d0bda72a6ddcb250a6d1a66172223787e52cec
BranislavMacosinec <branomps@gmail.com>
2017-08-25 13:59:30 +0100

    Repair last migration and add Dbset<cart> to dbContext.

--- ToolsStore/Models/ToolsStoreDB.cs
+++ ToolsStore/Models/ToolsStoreDB.cs
@@ -22,6 +22,7 @@ namespace ToolsStore.Models
         public DbSet<Product> Products { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Category> Categories { get; set; } 
+        public DbSet<Cart> Carts { get; set; }
     
     }
 }
