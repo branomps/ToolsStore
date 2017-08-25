7232811bfb814e1b4c8c083527dc05389d7a817c
BranislavMacosinec <branomps@gmail.com>
2017-08-25 13:55:56 +0100

    Add Class Cart To Models and Add Migration to database.

---
+++ ToolsStore/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToolsStore.Models
+{
+    public class Cart
+    {
+        [Key]
+        public virtual int RecordId { get; set; }
+        public virtual String CartId { get; set; }
+        public virtual int ProductId { get; set; }
+        public virtual int Count { get; set; }
+        public System.DateTime DateCreated { get; set; }
+
+        public virtual Product Product { get; set; }
+
+    }
+}
