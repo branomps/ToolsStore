9afbf34abed328f09f03bf4acd7bf94f160fe6ab
BranislavMacosinec <branomps@gmail.com>
2017-08-26 16:03:51 +0100

    Add more actiorResult methods to StoreController and Browse view.

--- ToolsStore/Controllers/StoreController.cs
+++ ToolsStore/Controllers/StoreController.cs
@@ -16,5 +16,33 @@ namespace ToolsStore.Controllers
             var categories = storeDB.Categories.ToList();
             return View(categories);
         }
+
+        public ActionResult Browse(string category)
+        {
+            // Retrieve Genre and its Associated Albums from database
+            var categoryModel = storeDB.Categories.Include("Product")
+                .Single(c => c.Name == category);
+
+            return View(categoryModel);
+        }
+        // GET: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var product = storeDB.Products.Find(id);
+
+            return View(product);
+        }
+
+        //
+        // GET: /Store/GenreMenu
+
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var category = storeDB.Categories.ToList();
+
+            return PartialView(category);
+        }
     }
 }
