06ad3a5d659ccf10b8c32d1be196286c639e0485
BranislavMacosinec <branomps@gmail.com>
2017-08-26 15:48:01 +0100

    Add Store Controller and Index view.

---
+++ ToolsStore/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToolsStore.Models;
+
+namespace ToolsStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        ToolsStoreDB storeDB = new ToolsStoreDB();
+        // GET: Store
+        public ActionResult Index()
+        {
+            var categories = storeDB.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
