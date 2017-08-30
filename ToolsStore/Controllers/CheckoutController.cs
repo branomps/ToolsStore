346b6057014e6a7712d32fc3bd3d9d966628d5af
BranislavMacosinec <branomps@gmail.com>
2017-08-30 09:07:49 +0100

    Add CheckOut Controller.

---
+++ ToolsStore/Controllers/CheckoutController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToolsStore.Models;
+
+namespace ToolsStore.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        ToolsStoreDB storeDB = new ToolsStoreDB(); 
+        // GET: Checkout
+        public ActionResult AddressPayment()
+        {
+            return View();
+        }
+
+        //Post : checkout
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new Order();
+            try
+            {
+                order.UserName = User.Identity.Name;
+                order.OrderDate = DateTime.Now;
+
+                //Save order
+                storeDB.Orders.Add(order);
+                storeDB.SaveChanges();
+
+                //Process the order
+                var cart = ShoppingCart.GetCart(this.HttpContext);
+                cart.CreateOrder(order);
+                return RedirectToAction("Complete", new { id = order.OrderId });
+            }
+            catch
+            {
+                return View(order);
+            }
+        }
+
+        // GET: /Checkout/Complete
+        public ActionResult Complete(int id)
+        {
+            // Validate customer owns this order
+            bool isValid = storeDB.Orders.Any( o => o.OrderId == id && o.UserName == User.Identity.Name);
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+    }
+}
